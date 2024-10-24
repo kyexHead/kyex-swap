426b92407d4ce4bf5464291e0c9ba619fea7d5a1
kakasonz112 <kakason.z211@gmail.com>
2024-10-24 17:20:03 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require('dotenv').config(); // Make sure to load env variables
 
 // addr: 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266;
 // const deployer =
 //   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const user1 =
-  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+// const user1 =
+//   "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -29,27 +30,27 @@ module.exports = {
     },
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
-      accounts: [
-        { privateKey: deployer, balance: "1000000000000000000000" }, // 1000 ETH
-        { privateKey: user1, balance: "1000000000000000000000" },
-      ],
-    },
+    // hardhat: {
+    //   chainId: 1337,
+    //   accounts: [
+    //     { privateKey: deployer, balance: "1000000000000000000000" }, // 1000 ETH
+    //     { privateKey: user1, balance: "1000000000000000000000" },
+    //   ],
+    // },
     zeta_test: {
       url: "https://zetachain-athens-evm.blockpi.network/v1/rpc/public",
       chainId: 7001,
-      accounts: [deployer],
+      accounts: [process.env.PRIVATE_KEY],
     },
     zeta_mainnet: {
       url: "https://zetachain-mainnet.g.allthatnode.com/full/evm/0a326da97ad5439c975b43721807e513",
       chainId: 7000,
-      accounts: [deployer],
+      accounts: [process.env.PRIVATE_KEY],
     },
     bsc_test: {
       url: "https://bsc-testnet.g.allthatnode.com/full/evm/e6f4078993be427386b109445e004b31",
       chainId: 97,
-      accounts: [deployer],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
--- script/test.js
+++ script/test.js
@@ -9,20 +9,25 @@ async function approveAndSwap() {
   );
   // const maxFeePerGas = await feedData.maxFeePerGas();
   // const maxPriorityFeePerGas = await feedData.maxPriorityFeePerGas();
-  const provider = new ethers.JsonRpcProvider(
-    "https://bsc-testnet.g.allthatnode.com/full/evm/e6f4078993be427386b109445e004b31"
+  const provider = new ethers.WebSocketProvider(
+    "wss://bsc-testnet.l.allthatnode.com/full/evm/0a326da97ad5439c975b43721807e513"
   );
   const feeData = await provider.getFeeData();
-  console.log(feeData);
+  console.log(feeData)
+//   console.log("lastBaseFeePerGas:", (feeData.lastBaseFeePerGas));
+//   console.log("maxFeePerGas:", (feeData.maxFeePerGas));
+//   console.log("maxPriorityFeePerGas:", (feeData.maxPriorityFeePerGas));
+//   console.log("gasPrice:", (feeData.gasPrice));
   const tx2 = await KYEXSwapBscV1.onReceiveSwap(
     "0xFa60D973F7642B748046464e165A65B7323b0DEE",
     "0x670f4f034B5e9B01580F888741d129866bBB2cC3",
     ethers.parseUnits("0.2", 18),
     feeData.gasPrice * BigInt(300000),
-    "0xe34d701947f33dfd3253c58f39f67ac5e3a51281a833963d0c9e4827a7e17915"
+    "0xe34d701947f33dfd3253c58f39f67ac5e3a51281a833963d0c9e4827a7e17915", {
+        gasLimit: 300000
+    }
   );
   console.log(tx2);
-  // const tx2 = await KYEXSwapBscV1.withdrawZETA();
 }
 
 approveAndSwap();
